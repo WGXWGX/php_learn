aaad7cdac3c12a24b1aed03b5f62cf071ea3ed0d
WGXWGX <wgx15145076862@sina.com>
2016-08-20 09:34:46 +0800

    Add files via upload

---
+++ 16-test.php
@@ -0,0 +1,44 @@
+<?php
+	$info=array(
+	'user'=>array(
+		array(1,'zhangsan',10,'nan'),
+		array(2,'lisi',12,'nv'),
+		array(3,'wangwu',13,'nan'),
+	),
+	'score'=>array(
+		array(1,100,90,80),
+		array(2,20,43,60),
+		array(3,54,67,21),
+	),
+	'connect'=>array(
+		array(1,'110','aaa@bb.com'),
+		array(2,'220','ccc@dd.com'),
+		array(3,'330','eee@ff.com'),
+	)
+);
+
+
+foreach($info as $tablename=>$table){
+	echo "<table border='1' align='center' width='800'>";
+	echo "<caption><h1>".$tablename."</h1></caption>";
+	foreach($table as $v){
+		echo "<tr>";
+		foreach($v as $value){
+			echo "<td>";
+			echo $value;
+			echo "</td>";
+		}
+		echo "</tr>";
+	}
+	echo "</table>";
+}
+	
+?>
+
+
+
+
+
+
+
+
